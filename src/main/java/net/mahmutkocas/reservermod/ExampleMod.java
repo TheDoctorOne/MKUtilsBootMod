c9711a0db5430087f2d4562e2b38e4942cd0fe8c
TheDoctorOne <mahmudkocas@gmail.com>
2023-07-22 17:20:08 +0300

    Defined Spring dependencies.

--- src/main/java/net/mahmutkocas/reservermod/ExampleMod.java
+++ src/main/java/net/mahmutkocas/reservermod/ExampleMod.java
@@ -1,31 +1,33 @@
-package com.example.examplemod;
+package net.mahmutkocas.reservermod;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import org.apache.logging.log4j.Logger;
+//import org.apache.logging.log4j.Logger;
 
 @Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
 public class ExampleMod
 {
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
-    public static final String VERSION = "1.0";
+    public static final String MODID = "reservermod";
+    public static final String NAME = "Mahmut Kocas Server Mod";
+    public static final String VERSION = "0.0.1";
 
-    private static Logger logger;
+//    private static Logger logger;
+
+
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
-        logger = event.getModLog();
+//        logger = event.getModLog();
     }
 
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
         // some example code
-        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+//        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
     }
 }
